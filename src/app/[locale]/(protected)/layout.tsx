281666df0fabcc4faf76e6712a8e18f6590f8f22
ArthNote <taehyungykim10@gmail.com>
2025-02-25 16:34:36 +0000

    Remove chat sidebar components and related types; update layout for protected pages and add chat page implementation with messaging functionality

--- src/app/[locale]/(protected)/layout.tsx
+++ src/app/[locale]/(protected)/layout.tsx
@@ -22,7 +22,7 @@ export default async function AdminLayout({
           <Separator orientation="vertical" className="h-4" />
           <BreadcrumbNavigation />
         </div>
-        <section className="p-4 mt-14">{children}</section>
+        <section className="p-4 pt-2 mt-14">{children}</section>
       </main>
     </SidebarProvider>
   );
--- src/components/sidebar/workspace_switcher.tsx
+++ src/components/sidebar/workspace_switcher.tsx
@@ -40,9 +40,9 @@ export function WorkspaceSwitcher({
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton
               size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              className="data-[state=open]:bg-sidebar data-[state=open]:text-sidebar-accent-foreground"
             >
-              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-sidebar-primary-foreground">
                 <activeTeam.logo className="size-4" />
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
